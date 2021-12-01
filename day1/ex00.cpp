cd15b56fea08b87b0fcb6a52d8d9e5412aff84da
LJmartin94 <lindsayjamesmartin94@gmail.com>
2021-12-01 14:17:24 +0100

    Can now read the input file and print it to the standard out

--- day1/ex00.cpp
+++ day1/ex00.cpp
@@ -1,9 +1,27 @@
-#include<iostream>
+#include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int	main() 
 {
-	cout<<"Hello World\n";
+	fstream file;
+	streampos size;
+	char *mem;
+
+	file.open("input.txt");
+	if (file.is_open())
+	{
+		file.seekg (0, ios::end);
+		size = file.tellg();
+		mem = new char [size];
+
+		file.seekg (0, ios::beg);
+		file.read (mem, size);
+		file.close();
+	}
+	else
+		cout << "File didn't open\n";
+	cout<< mem;
 	return 0;
 }
