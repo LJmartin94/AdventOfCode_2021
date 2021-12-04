9abfc37093bf2bbaebefb68699eff0d5d153bec6
LJmartin94 <lindsayjamesmartin94@gmail.com>
2021-12-04 19:06:16 +0100

    thought I had the answer but its wrong, so this was my first attempt.

--- day4/ex00.py
+++ day4/ex00.py
@@ -1,7 +1,73 @@
+def calc_winning_score(current_num, winning_board):
+    # print("Winning board:\n" + str(winning_board))
+    # print("Winning num:\n" + str(current_num))
+    unmarked_total = 0
+    for y in range(5):
+        for x in range(5):
+            if winning_board[y][x] != 'x':
+                unmarked_total += int(winning_board[y][x])
+    print(str(unmarked_total))
+    print(str(current_num))
+    score = int(unmarked_total) * int(current_num)
+    return score
+
+
+def check_all_boards(all_boards):
+    # # check rows
+    b = 0
+    for board in all_boards:
+        r = 0
+        for row in board:
+            win_score = 0
+            n = 0
+            for number in row:
+                if number == 'x':
+                    win_score += 1
+                n += 1
+            if win_score == 5:
+                return b
+            r += 1
+        b += 1
+    # check columns
+    b = 0
+    for board in all_boards:
+        for x in range(5):
+            win_score = 0
+            for y in range(5):
+                if board[y][x] == 'x':
+                    win_score += 1
+            if win_score == 5:
+                return b
+        b += 1
+    return -1
+
+
+
 def find_winning_board(numbers_drawn, all_boards):
-    print(numbers_drawn)
     print(all_boards)
-    return (0)
+    numbers_drawn = numbers_drawn.split(',')
+    print(numbers_drawn)
+
+    # Mark of current_num with x
+    for i in range(len(numbers_drawn)):
+        current_num = numbers_drawn[i]
+        b = 0
+        for board in all_boards:
+            r = 0
+            for row in board:
+                n = 0
+                for number in row:
+                    if number == current_num:
+                        all_boards[b][r][n] = 'x'
+                    n += 1
+                r += 1
+            b += 1
+        # After every number is marked off, check if any boards have won
+        winning_board = check_all_boards(all_boards)
+        print("Winning board index: " + str(winning_board))
+        if winning_board != -1:
+            return calc_winning_score(current_num, all_boards[winning_board])
+    return 0
 
 
 def main():
