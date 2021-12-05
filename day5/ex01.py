99588c0d19bdaa953021ba01c29a5943a1564099
LJmartin94 <lindsayjamesmartin94@gmail.com>
2021-12-05 11:36:42 +0100

    day 5.1 solved

--- day5/ex01.py
+++ day5/ex01.py
@@ -36,6 +36,42 @@ def draw_straight_lines(grid, op_array):
     return grid
 
 
+def is_45_diagonal(xa, xb, ya, yb):
+    xdif = (xa - xb)
+    ydif = (ya - yb)
+    xdif *= xdif
+    ydif *= ydif
+    if xdif == ydif:
+        return 1
+    else:
+        return 0
+
+
+def draw_diagonal_lines(grid, op_array):
+    for op in op_array:
+        xa = int(op[0][0])
+        xb = int(op[1][0])
+        ya = int(op[0][1])
+        yb = int(op[1][1])
+        if is_45_diagonal(xa, xb, ya, yb) == 1:
+            # Make sure coord xa,ya describes left most point of diagonal line
+            if xa > xb:
+                swap = xa
+                xa = xb
+                xb = swap
+                swap = ya
+                ya = yb
+                yb = swap
+            # Check if point a to b is a downward slope from left to right, or upward
+            if ya > yb:
+                ymod = -1
+            else:
+                ymod = 1
+            for i in range(xb - xa + 1):
+                grid[xa + i][ya + (i * ymod)] += 1
+    return grid
+
+
 def main():
     file = open("input.txt")
     line = [0 for x in range(1000)]
@@ -53,6 +89,7 @@ def main():
         op_array.append(op)
 
     grid = draw_straight_lines(grid, op_array)
+    grid = draw_diagonal_lines(grid, op_array)
     for y in range(1000):
         for x in range(1000):
             if grid[x][y] >= 2:
