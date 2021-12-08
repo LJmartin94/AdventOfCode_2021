f22b9af69d7538ba40ab1bc32405a9d88231c286
LJmartin94 <lindsayjamesmartin94@gmail.com>
2021-12-08 17:30:05 +0100

    adding ex00 as base for ex01

---
+++ day8/ex01.py
@@ -0,0 +1,38 @@
+from enum import IntEnum
+
+
+class Segments(IntEnum):
+    ZERO = 6
+    ONE = 2
+    TWO = 5
+    THREE = 5
+    FOUR = 4
+    FIVE = 5
+    SIX = 6
+    SEVEN = 3
+    EIGHT = 7
+    NINE = 6
+
+
+def main():
+    file = open("test_input.txt")
+    output_vals = []
+
+    for line in file:
+        output_line = line.split(' | ')[1].split('\n')[0].split()
+        output_vals.append(output_line)
+
+    easy_digits = 0
+    for line in output_vals:
+        for digit in line:
+            if len(digit) == Segments.ONE or len(digit) == Segments.FOUR or \
+            len(digit) == Segments.SEVEN or len(digit) == Segments.EIGHT:
+                easy_digits += 1
+
+    answer = easy_digits
+    print("Answer to 8.1:")
+    print(answer)
+
+
+if __name__ == "__main__":
+    main()
