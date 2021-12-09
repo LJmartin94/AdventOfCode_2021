7a3c881d2a5be649f9960a763999d8eb4a37a391
LJmartin94 <lindsayjamesmartin94@gmail.com>
2021-12-09 16:40:05 +0100

    Exercise two, return of the floodfill-boogaloo

--- day9/ex01.py
+++ day9/ex01.py
@@ -1,3 +1,29 @@
+def find_basin_size(point, grid, ydims, xdims, basinsize):
+    y = point[0]
+    x = point[1]
+    north = (y - 1, x)
+    south = (y + 1, x)
+    west = (y, x - 1)
+    east = (y, x + 1)
+
+    if grid[y][x] != '9':
+        basinsize += 1
+        grid[y][x] = '9'
+    if north[0] >= 0:
+        if grid[north[0]][north[1]] != '9':
+            basinsize = find_basin_size(north, grid, ydims, xdims, basinsize)
+    if south[0] < ydims:
+        if grid[south[0]][south[1]] != '9':
+            basinsize = find_basin_size(south, grid, ydims, xdims, basinsize)
+    if west[1] >= 0:
+        if grid[west[0]][west[1]] != '9':
+            basinsize = find_basin_size(west, grid, ydims, xdims, basinsize)
+    if east[1] < xdims:
+        if grid[east[0]][east[1]] != '9':
+            basinsize = find_basin_size(east, grid, ydims, xdims, basinsize)
+    return basinsize
+
+
 def check_lowpoint(y, x, grid, ydims, xdims):
     lowpoint = 1
     north = (y - 1, x)
@@ -25,21 +51,26 @@ def find_lowest_points(grid, ydims, xdims):
     for y in range(ydims):
         for x in range(xdims):
             if check_lowpoint(y, x, grid, ydims, xdims) == 1:
-                list_lowests.append(int(grid[y][x]))
+                list_lowests.append((y, x))
     return list_lowests
 
 
 def main():
-    file = open("test_input.txt")
-    ydims = 5
+    file = open("input.txt")
+    ydims = 100
     grid = [[] for _ in range(ydims)]
 
     for y in range(ydims):
-        grid[y] = file.readline().split('\n')[0]
-    xdims = len(grid[0])
+        grid[y] = list(file.readline().split('\n')[0])
+    xdims = len(grid[y])
 
     list_lowests = find_lowest_points(grid, ydims, xdims)
-    answer = sum(list_lowests) + len(list_lowests)
+    basin_sizes = []
+    for point in list_lowests:
+        basin_size = find_basin_size(point, grid.copy(), ydims, xdims, 0)
+        basin_sizes.append(basin_size)
+    basin_sizes.sort(reverse=True)
+    answer = basin_sizes[0] * basin_sizes[1] * basin_sizes[2]
     print("Answer to 9.1:")
     print(answer)
 
