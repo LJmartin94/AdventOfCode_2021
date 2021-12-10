8192ae0fc04f0bb7678c0eb72c82bddc25a50a55
LJmartin94 <lindsayjamesmartin94@gmail.com>
2021-12-10 13:47:30 +0100

    Ex00 finished

--- day10/ex00.py
+++ day10/ex00.py
@@ -1,6 +1,31 @@
 def main():
-    file = open("test_input.txt")
-    answer = 0
+    file = open("input.txt")
+
+    error = 0
+    line_number = 0
+    for line in file:
+        line_number += 1
+        parsed_line = []
+        for char in line:
+            if char == '(' or char == '[' or char == '{' or char == '<':
+                parsed_line.append(char)
+            i = len(parsed_line) - 1
+            if char == ')' or char == ']' or char == '}' or char == '>':
+                if char == ')' and parsed_line[i] != '(':
+                    error += 3
+                    break
+                elif char == ']' and parsed_line[i] != '[':
+                    error += 57
+                    break
+                elif char == '}' and parsed_line[i] != '{':
+                    error += 1197
+                    break
+                elif char == '>' and parsed_line[i] != '<':
+                    error += 25137
+                    break
+                else:
+                    parsed_line.pop(i)
+    answer = error
     print("Answer to 10.0:")
     print(answer)
 
