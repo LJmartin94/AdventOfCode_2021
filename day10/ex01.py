8e48d6047726c523b0a2eddbc9d5e00622f28b1b
LJmartin94 <lindsayjamesmartin94@gmail.com>
2021-12-10 14:48:11 +0100

    ex01 done

--- day10/ex01.py
+++ day10/ex01.py
@@ -1,9 +1,10 @@
 def main():
-    file = open("test_input.txt")
+    file = open("input.txt")
 
-    error = 0
+    solvable_lines = []
     line_number = 0
     for line in file:
+        error = 0
         line_number += 1
         parsed_line = []
         for char in line:
@@ -12,20 +13,42 @@ def main():
             i = len(parsed_line) - 1
             if char == ')' or char == ']' or char == '}' or char == '>':
                 if char == ')' and parsed_line[i] != '(':
-                    error += 3
+                    error = 1
                     break
                 elif char == ']' and parsed_line[i] != '[':
-                    error += 57
+                    error = 1
                     break
                 elif char == '}' and parsed_line[i] != '{':
-                    error += 1197
+                    error = 1
                     break
                 elif char == '>' and parsed_line[i] != '<':
-                    error += 25137
+                    error = 1
                     break
                 else:
                     parsed_line.pop(i)
-    answer = error
+        if error == 0:
+            solvable_lines.append(parsed_line)
+
+    line_scores = []
+    for i in range(len(solvable_lines)):
+        score = 0
+        while len(solvable_lines[i]) > 0:
+            c = len(solvable_lines[i]) - 1
+            score *= 5
+            if solvable_lines[i][c] == '(':
+                score += 1
+            if solvable_lines[i][c] == '[':
+                score += 2
+            if solvable_lines[i][c] == '{':
+                score += 3
+            if solvable_lines[i][c] == '<':
+                score += 4
+            # print(str(solvable_lines[i]) + str(score))
+            solvable_lines[i].pop(c)
+        line_scores.append(score)
+
+    line_scores.sort()
+    answer = line_scores[int(len(line_scores)/2)]
     print("Answer to 10.1:")
     print(answer)
 
