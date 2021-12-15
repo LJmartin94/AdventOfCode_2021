2f55466985acdfa4a27f6abaa5db6c9e09d2ee28
LJmartin94 <lindsayjamesmartin94@gmail.com>
2021-12-15 22:05:42 +0100

    Was putting off solving day12 because I knew I'd have to learn about python classes (immediately realised it would be 'easy to solve' with C structs, and that classes were probably the python equivalent). Actually solved ex00 by modifying my input values too xD. The cave now named 'sx' was originally called 'st', and thus gave a false positive for 'having already occurred' in a path because it was being found in 'start'. Didnt take long to figure out though as I used the all the test input first and those were all working fine, so I knew it must be an input peculiarity.

---
+++ day12/ex00.py
@@ -0,0 +1,90 @@
+class Cave:
+    def __init__(self, cave_id, cave_size):
+        self.cave_id = cave_id
+        self.cave_size = cave_size
+        self.connected_caves = []
+
+
+def traverse_caves(cave_str, cave_array, path, all_paths):
+    cave = go_to_cave(cave_str, cave_array)
+    if cave_str == 'end':
+        return
+    for next_cave in cave.connected_caves:
+        if next_cave.isupper() or str(path).find(next_cave) == -1:
+            p = path.copy()
+            p.append(next_cave)
+            traverse_caves(next_cave, cave_array, p, all_paths)
+            all_paths.append(p)
+
+
+def go_to_cave(cave_str, cave_array):
+    for cave in cave_array:
+        if cave.cave_id == cave_str:
+            return cave
+    return 0
+
+
+def not_already_included(cave_ids, cave):
+    for i in range(len(cave_ids)):
+        if cave_ids[i] == cave:
+            return 0
+    return 1
+
+
+def main():
+    file = open("input.txt")
+    cave_pairs = []
+    for line in file:
+        cave_pairs.append(line.split('\n')[0].split('-'))
+
+    cave_ids = []
+    for pair in cave_pairs:
+        for cave in pair:
+            if not_already_included(cave_ids, cave) == 1:
+                cave_ids.append(cave)
+
+    cave_array = []
+    for cave in cave_ids:
+        size = cave.isupper()
+        if size:
+            size = "big"
+        else:
+            size = "small"
+        cave_array.append(Cave(cave, size))
+
+    for cave in cave_array:
+        for pair in cave_pairs:
+            if pair[0] == cave.cave_id:
+                cave.connected_caves.append(pair[1])
+            if pair[1] == cave.cave_id:
+                cave.connected_caves.append(pair[0])
+
+    for cave in cave_array:
+        print(cave.cave_id)
+        print(cave.cave_size)
+        print(cave.connected_caves)
+        print()
+
+    path = []
+    path.append('start')
+    all_paths = []
+    traverse_caves('start', cave_array, path, all_paths)
+    i = len(all_paths) - 1
+    while i >= 0:
+        last_elem = len(all_paths[i]) - 1
+        if all_paths[i][last_elem] != 'end':
+            all_paths.pop(i)
+        i -= 1
+
+    number_of_paths = 0
+    for path in all_paths:
+        number_of_paths += 1
+        print(path)
+
+    answer = number_of_paths
+    print("Answer to 12.0:")
+    print(answer)
+
+
+if __name__ == "__main__":
+    main()
