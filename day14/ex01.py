87b6965775e09e01e2ffd7a5251d304869f50f28
LJmartin94 <lindsayjamesmartin94@gmail.com>
2021-12-21 11:34:02 +0100

    Using day 14 ex00 as base for ex01, but have already realised its nowhere near powerful enough to simulate 40 steps, will have to think of a different approach

---
+++ day14/ex01.py
@@ -0,0 +1,44 @@
+from collections import Counter
+
+
+def insertion_process(polymer, rules, steps):
+    for s in range(steps):
+        i = 0
+        while i < (len(polymer) - 1):
+            for rule in rules:
+                if polymer[i] == rule[0] and polymer[i+1] == rule[1]:
+                    i += 1
+                    polymer = polymer[:i] + rule[6] + polymer[i:]
+                    break
+            i += 1
+        print(str(s + 1) + ": " + str(i + 1))
+    return polymer
+
+
+def main():
+    lines = open("test_input.txt").read().split('\n')
+    polymer = ""
+    rules = []
+    breaker = 0
+    for line in lines:
+        if line == '':
+            breaker = 1
+            continue
+        if breaker == 0:
+            polymer = line
+        if breaker == 1:
+            rules.append(line)
+
+    steps = 40
+    polymer = insertion_process(polymer, rules, steps)
+    elements = Counter(polymer)
+    elements = elements.most_common(len(elements))
+    most_common = elements[0][1]
+    least_common = elements[len(elements) - 1][1]
+    answer = most_common - least_common
+    print("Answer to 14.1:")
+    print(answer)
+
+
+if __name__ == "__main__":
+    main()
