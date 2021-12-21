35de5b5155a2a4435cb4f55982c99180d7cab6c2
LJmartin94 <lindsayjamesmartin94@gmail.com>
2021-12-21 13:47:29 +0100

    Added files for day 15, time to learn how to write an A* algorithm I guess

---
+++ day15/ex00.py
@@ -0,0 +1,10 @@
+def main():
+    lines = open("test_input.txt").read().split('\n')
+
+    answer = lines
+    print("Answer to 15.0:")
+    print(answer)
+
+
+if __name__ == "__main__":
+    main()
