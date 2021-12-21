f3ece622d4b4ce00df80ff40c8991ace328034cc
LJmartin94 <lindsayjamesmartin94@gmail.com>
2021-12-21 13:34:29 +0100

    Implemented my solution and it works :D

--- day14/ex01.py
+++ day14/ex01.py
@@ -1,45 +1,36 @@
 from collections import Counter
 
 
-def single_step(polymer, rules):
-    i = 0
-    while i < (len(polymer) - 1):
-        for rule in rules:
-            if polymer[i] == rule[0] and polymer[i + 1] == rule[1]:
-                i += 1
-                polymer = polymer[:i] + rule[6] + polymer[i:]
-                break
-        i += 1
-    return polymer
+def convert_pairs_to_letters(pairs):
+    letters = []
+    letters = Counter(letters)
+    for p in range(len(pairs)):
+        first_letter = pairs.most_common(len(pairs))[p][0][0]
+        value = pairs.most_common(len(pairs))[p][1]
+        letters.update({first_letter: value})
+    return letters
 
 
-def insertion_process(polymer, rules, steps):
-    polymer = single_step(polymer, rules)
-    if steps > 1:
-        polymer = extended_insertion_process(polymer, rules, (steps - 1))
-    return polymer
-
-
-def extended_insertion_process(polymer, rules, steps):
-    # if (steps >= 5):
-    #     print("steps to go: " + str(steps))
-    segments = len(polymer) - 1
-    polymer_segments = []
-    polymer_solved = []
-    for s in range(segments):
-        polymer_segments.append(str(polymer[s] + polymer[s+1]))
-        polymer_solved.append(insertion_process(polymer_segments[s], rules, steps))
-        # print(polymer_solved)
-    for s in range(len(polymer_solved)):
-        if s > 0:
-            polymer_solved[s] = polymer_solved[s][1:]
-    polymer = ''.join(polymer_solved)
-    # print(polymer)
-    return polymer
+def update_pairs(pairs, rules, steps):
+    for s in range(steps):
+        changes = []
+        changes = Counter(changes)
+        for pair in pairs:
+            for rule in rules:
+                if rule.find(pair) != -1:
+                    insertion = rule[6]
+                    old_pair = pair
+                    new_pairA = str(pair[0] + insertion)
+                    new_pairB = str(insertion + pair[1])
+                    changes.update({new_pairA: pairs[old_pair]})
+                    changes.update({new_pairB: pairs[old_pair]})
+                    changes.update({old_pair: (pairs[old_pair] * -1)})
+        pairs.update(changes)
+    return pairs
 
 
 def main():
-    lines = open("test_input.txt").read().split('\n')
+    lines = open("input.txt").read().split('\n')
     polymer = ""
     rules = []
     breaker = 0
@@ -53,9 +44,16 @@ def main():
             rules.append(line)
 
     steps = 40
-    polymer = extended_insertion_process(polymer, rules, steps)
-    elements = Counter(polymer)
-    elements = elements.most_common(len(elements))
+    pairs = []
+    for i in range(len(polymer) - 1):
+        pairs.append(str(polymer[i]) + str(polymer[i+1]))
+    pairs = Counter(pairs)
+    pairs = update_pairs(pairs, rules, steps)
+    letters = convert_pairs_to_letters(pairs)
+    last_letter = polymer[len(polymer) - 1]
+    letters.update({last_letter: 1})
+
+    elements = letters.most_common(len(letters))
     most_common = elements[0][1]
     least_common = elements[len(elements) - 1][1]
     answer = most_common - least_common
