5fbcd7821348207d2c6f4e464b5ab19220164eef
LJmartin94 <lindsayjamesmartin94@gmail.com>
2021-12-26 16:30:36 +0100

    Day 25 done

--- day25/ex00.py
+++ day25/ex00.py
@@ -1,21 +1,57 @@
+import copy
+
+
+def print_map(cucumber_map):
+    for row in cucumber_map:
+        string = ''.join([str(elem) for elem in row])
+        print(string)
+    print()
+    return
+
+
+def simulate_step(cm, dimy, dimx):
+    moved = 0
+    ccm = copy.deepcopy(cm)
+    for y in range(dimy):
+        for x in range(dimx):
+            if ccm[y][x] == '>' and ccm[y][(x+1) % dimx] == '.':
+                moved += 1
+                cm[y][x] = '.'
+                cm[y][(x+1) % dimx] = '>'
+    ccm = copy.deepcopy(cm)
+    for y in range(dimy):
+        for x in range(dimx):
+            if ccm[y][x] == 'v' and ccm[(y+1) % dimy][x] == '.':
+                moved += 1
+                cm[y][x] = '.'
+                cm[(y+1) % dimy][x] = 'v'
+    # print_map(cm)
+    print(moved)
+    return (moved, cm)
+
 def main():
-    lines = open("test_input.txt").read().split('\n')
+    lines = open("input.txt").read().split('\n')
     dimy = len(lines)
     dimx = len(lines[0])
 
-    map = [[0 for _ in range(dimx)] for _ in range(dimy)]
+    cucumber_map = [[0 for _ in range(dimx)] for _ in range(dimy)]
     y = 0
     for line in lines:
         x = 0
         for char in line:
-            map[y][x] = char
+            cucumber_map[y][x] = char
             x += 1
         y += 1
 
-    for row in map:
-        print(row)
+    steps = 0
+    moved = 1
+    while moved != 0:
+        tup = simulate_step(cucumber_map, dimy, dimx)
+        moved = tup[0]
+        cucumber_map = tup[1]
+        steps += 1
 
-    answer = lines
+    answer = steps
     print("Answer to 25.0:")
     print(answer)
 
